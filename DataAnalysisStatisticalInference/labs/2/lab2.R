8d56be32a4196dba7a9fdfeecb685ce8b76fb808
jose <jose@promedon.we7.local>
2014-09-24 15:04:01 +0100

    more stuff for data analysis course.

--- DataAnalysisStatisticalInference/labs/2/lab2.R
+++ DataAnalysisStatisticalInference/labs/2/lab2.R
@@ -26,14 +26,39 @@ sample(outcomes, size = 1, replace = TRUE)
 sample(outcomes, size = 1, replace = TRUE)
 
 
-# fair coin with 100 flips
+# simulating: fair coin with 100 flips
 outcomes = c("heads", "tails")
 sim_fair_coin = sample(outcomes, size = 100, replace = TRUE)
 sim_fair_coin
 table(sim_fair_coin)
 
 
+# simulating: unfair coin with 100 flips
+outcomes = c("heads", "tails")
+sim_unfair_coin = sample(outcomes, size = 100, replace = TRUE, prob = c(0.2, 0.8))
+sim_unfair_coin
+table(sim_unfair_coin)
 
 
+# simulating: indepedent shooter
+outcomes = c("H", "M")
+sim_basket = sample(outcomes, size = 133, replace = TRUE, prob = c(0.45, 0.55))
+sim_basket
+table(sim_basket)
+
+
+## Comparing simulated versus Kobe
+kobe_streak = calc_streak(kobe$basket)
+sim_streak = calc_streak(sim_basket)
+
+summary(kobe_streak)
+summary(sim_streak)
+
+kobe_table = table(kobe_streak)
+sim_table = table(sim_streak)
+
+par(mfrow = c(1,2))
+barplot(kobe_table, main="Kobe")
+barplot(sim_table, main="Simulated")
 
 
---
+++ DataAnalysisStatisticalInference/labs/2/quiz2.R
@@ -0,0 +1,7 @@
+
+## probability of finding exactly 92 out of 100 items with property X when
+## 90% of all items have property X
+dbinom(92, size=100, p = 0.9)
+
+
+
