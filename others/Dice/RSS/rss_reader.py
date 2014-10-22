38bc5eb7baad5362b45f83a7861211d7b131f437
jose <jose@jose-Aspire-5050.(none)>
2014-10-22 23:01:05 +0100

    add some changes

--- others/Dice/RSS/rss_reader.py
+++ others/Dice/RSS/rss_reader.py
@@ -12,31 +12,40 @@
 #       - http://tedunderwood.com/2012/08/14/where-to-start-with-text-mining/
 #       - "http://www.wordle.net/" 
 #
+#
+# other insights:
+#   - compare agents overall and over time 
+#
 
 import feedparser
 
 baseurl = 'http://www.allagents.co.uk/rss/{agency}/'
-#agencies = ['richard-worth', 'redlet', 'reed-residential']
-agencies = ['redlet']
+#agencies = ['richard-worth', 'redlet', 'reed-residential', 'choices']
+#agencies = ['redlet']
+agencies = ['choices']
 
 
 for agent in agencies:
     url = baseurl.format(agency=agent)
     d = feedparser.parse(url)
     #print d
-    #print len(d['entries'])
+    print len(d['entries'])
     
-    ## ## to get the date and rating
+    ## ## the whole record
     ## for post in d.entries:
-    ##     #print post
-    ##     print post.title
+    ##     print post
     ##     print "------------------------------------------------------------------------------"
+    
+    
+    ## to get the date and rating
+    for post in d.entries:
+        print post.title
+        print "------------------------------------------------------------------------------"
     
 
     
     ## ## to get the date and rating
     ## for post in d.entries:
-    ##     #print post
     ##     print post.summary
     ##     print "------------------------------------------------------------------------------"
     
