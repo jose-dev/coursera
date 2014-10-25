3850dd5b0b57f4fd9b7f1a793f57501a90a60f94
jose <jose@jose-Aspire-5050.(none)>
2014-10-25 12:49:56 +0100

    clean up

--- others/Dice/RSS/rss_collector.py
+++ others/Dice/RSS/rss_collector.py
@@ -1,20 +1,9 @@
 #
-# RSS obtained from http://www.allagents.co.uk/r/
+# RSS obtained from http://www.allagents.co.uk/rss/
 #
 # python docs:
 #   - http://www.pythonforbeginners.com/feedparser/using-feedparser-in-python
 #
-#
-# content analysis:
-#   - ideas:
-#       - split reviews according to ratings (analyse them separetely)
-#   - resourses:
-#       - http://tedunderwood.com/2012/08/14/where-to-start-with-text-mining/
-#       - "http://www.wordle.net/" 
-#
-#
-# other insights:
-#   - compare agents overall and over time 
 #
 
 import re
@@ -22,8 +11,6 @@ import feedparser
 from datetime import datetime
 
 BASEURL = 'http://www.allagents.co.uk/rss/{agency}/'
-#agencies = ['richard-worth', 'redlet', 'reed-residential', 'choices']
-#agencies = ['redlet']
 agencies = ['choices',
             'manning-stainton',
             'reeds-rains',
@@ -58,25 +45,27 @@ def extract_reviewer(title):
         return m.group(1)
 
 
-# print header
-print "\t".join(['agent', 'date', 'rating', 'reviewer', 'summary'])
-
-## print formatted rss data
-for agent in agencies:
-    url = BASEURL.format(agency=agent)
-    d = feedparser.parse(url)
-
-    for post in d.entries:
-        title    = post.title
-        summary  = post.summary.replace('\n', ' ').replace('\r', '')
-        rating   = extract_rating(title)
-        reviewer = extract_reviewer(title)
-        dt       = extract_date(title)
-        if not rating or not reviewer or not dt:
-            continue
-        else:    
-            try:
-                print "\t".join([str(val) for val in [agent, dt, rating, reviewer, summary]])
-            except Exception:
-                pass
+if __name__ == '__main__':
+    
+    # print header
+    print "\t".join(['agent', 'date', 'rating', 'reviewer', 'summary'])
+    
+    ## print formatted rss data
+    for agent in agencies:
+        url = BASEURL.format(agency=agent)
+        d = feedparser.parse(url)
+    
+        for post in d.entries:
+            title    = post.title
+            summary  = post.summary.replace('\n', ' ').replace('\r', '')
+            rating   = extract_rating(title)
+            reviewer = extract_reviewer(title)
+            dt       = extract_date(title)
+            if not rating or not reviewer or not dt:
+                continue
+            else:    
+                try:
+                    print "\t".join([str(val) for val in [agent, dt, rating, reviewer, summary]])
+                except Exception:
+                    pass
 
